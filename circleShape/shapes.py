cbc5280bd9bb2b1139120a4c6c95857d7286b06f
mswarankit <ankitnightr14@gmail.com>
2024-10-19 00:13:54 +0530

    added all shapes

--- circleShape/shapes.py
+++ circleShape/shapes.py
@@ -14,6 +14,41 @@ class Shape:
         print(f"Shape: {self.name}\nArea: {self.calculate_area():.2f}\nVolume: {self.calculate_volume():.2f}\n")
 
 
+class Triangle(Shape):
+    def __init__(self, base, height):
+        super().__init__(name="Triangle")
+        self.base = base
+        self.height = height
+    
+    def calculate_area(self):
+        return (1/2)* self.base * self.height
+    
+    def calculate_volume(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__(name="Rectangle")
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+    
+    def calculate_volume(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, side):
+        super().__init__(name="Square")
+        self.side = side
+    
+    def calculate_area(self):
+        return math.pow(self.side, 2)
+    
+    def calculate_volume(self):
+        return 0
+    
 
 class Circle(Shape):
     def __init__(self, radius):
@@ -25,8 +60,29 @@ class Circle(Shape):
 
     def calculate_volume(self):
         return 0
-    
 
+class Cube(Square):
+    def __init__(self, side):
+        super().__init__(side=side)
+        self.name = "Cube"
+    
+    def calculate_area(self):
+        return 6 * super().calculate_area()
+    
+    def calculate_volume(self):
+        return math.pow(self.side, 3)
+
+class Cuboid(Rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+        self.name = "Cuboid"
+    
+    def calculate_area(self):
+        return 2*(super().calculate_area() + (self.length * self.height) + (self.height * self.width))
+    
+    def calculate_volume(self):
+        return super().calculate_area()*self.height
 
 class Cylinder(Circle):
     def __init__(self, radius,height):
@@ -66,14 +122,32 @@ class Sphere(Circle):
     def calculate_volume(self):
         return (4/3)*super().calculate_area() * self.radius
     
-
+class Hemishpere(Circle):
+    def __init__(self, radius):
+        super().__init__(radius)
+        self.name = "Hemishpere"
+    
+    def calculate_area(self):
+        return 3 * super().calculate_area()
+    
+    def calculate_volume(self):
+        return (2/3)*super().calculate_volume()
+    
+# 2d shapes
+tri = Triangle(12, 7)
+rect = Rectangle(15, 19)
+squa = Square(16)
 c = Circle(15)
+# 3d shpaes
 cyl = Cylinder(10, 12)
 cone = Cone(12, 15)
 sp = Sphere(5)
+cu = Cube(6)
+cuboid = Cuboid(12, 15, 11)
+hsp = Hemishpere(4)
 
 
-# shp = [c, cyl, cone, sp]
+# shp = [tri, rect, squa, c, cyl, cone, sp, cu, cuboid, hsp]
 # for i in shp:
 #     i.printAll()
 
